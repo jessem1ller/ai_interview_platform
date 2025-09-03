cd82f75dd93b52926896a1bb7b4f44104df2627e
Jesse Miller <silverhonda84@gmail.com>
2025-09-03 07:30:49 -0700

    minor change

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -32,7 +32,7 @@ async function Home() {
           </p>
 
           <Button asChild className="btn-primary max-sm:w-full">
-            <Link href="/interview/new">Start an Interview</Link>
+            <Link href="/interview/new">Create an Interview</Link>
           </Button>
         </div>
 
